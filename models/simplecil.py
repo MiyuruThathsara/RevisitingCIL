967fbae8cf64abd0325beb4e8ac124fdf9304a54
Miyuru Thathsara <mthathsara@outlook.com>
2023-10-17 23:10:23 +0800

    Number of epochs increased to 20

--- models/simplecil.py
+++ models/simplecil.py
@@ -99,7 +99,7 @@ class Learner(BaseLearner):
             scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.args['tuned_epoch'], eta_min=self.args["min_lr"])
             # scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
 
-            for epoch in range(10):
+            for epoch in range(20):
                 for i, batch in enumerate(trainloader):
                     (_,data,label)=batch
                     data=data.cuda()
